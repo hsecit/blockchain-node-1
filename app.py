55b91420f86ee4d5cd9b16839ec46f3a228a0b57
hamza windows <you@example.com>
2020-07-12 18:18:18 +0100

    fixerror

--- app.py
+++ app.py
@@ -31,12 +31,7 @@ peers = set()
 @app.route('/new_transaction', methods=['POST'])
 def new_transaction():
     tx_data = request.get_json()
-    required_fields = ["author", "content"]
-
-    for field in required_fields:
-        if not tx_data.get(field):
-            return "Invalid transaction data", 404
-
+    
     tx_data["timestamp"] = time.time()
 
     blockchain.add_new_transaction(tx_data)
