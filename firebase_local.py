6aa089ec0a1496b1e4a5cb27b988671098dc7e33
hamza windows <you@example.com>
2020-07-12 17:59:09 +0100

    ready to deploy node1

---
+++ firebase_local.py
@@ -0,0 +1,28 @@
+import pyrebase
+
+config = {
+    "apiKey": "AIzaSyDi8BQjZHM0l2n1tEX5DukmyE0jQ2kctEQ",
+    "authDomain": "cryptography-project-12233.firebaseapp.com",
+    "databaseURL": "https://cryptography-project-12233.firebaseio.com",
+    "projectId": "cryptography-project-12233",
+    "storageBucket": "cryptography-project-12233.appspot.com",
+    "messagingSenderId": "801611067749",
+    "appId": "1:801611067749:web:19e03a535319b18cb7718c",
+    "measurementId": "G-LSEMCCBFPT"
+}
+
+fire = pyrebase.initialize_app(config)
+
+
+def store_blockchain(data):
+    """
+    this function store transactions in firebase
+    :param data:
+    :return:
+    """
+    dbase = fire.database()
+    DbRef = dbase.child("Nodes")
+    # the data will be the transaction(index,prev_hash,Nonce,pow,data,timestamp)
+    # integrate the markle tree and private key
+    DbRef.push(data)
+
